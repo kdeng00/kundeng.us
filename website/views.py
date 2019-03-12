ca18277ead9300bb249bfcedb1fd4f006e888e6b
amazing-username <kdeng02@aurora.edu>
2019-03-11 21:49:59 -0400

    Blank slate of the project #7

--- website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def homepage(request):
-    title = 'Kun Deng'
-    return render(request, 'homepage.html', {'title':title})
-
-def contact(request):
-    title = 'Kun Deng - Contact'
-    return render(request, 'contact.html', {'title':title})
