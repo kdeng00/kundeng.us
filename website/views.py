ef9ab6c952f8ed518ef685d9bc3fc9d1a58b6041
kdeng00 <kunden94@gmail.com>
2021-08-22 19:54:42 -0400

    Cleanup

--- website/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def test(request):
-    return render(request, 'website/test.html')
-
-def home(request):
-    return render(request, 'website/home.html')
